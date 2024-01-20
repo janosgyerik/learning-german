fc132e1784d7f9956f9ae95433f90e22393f1bd5
Janos Gyerik <info+kraken@janosgyerik.com>
2024-01-20 12:25:30 +0100

    Add ch01-01-vorstellungsgespraech.flashcards

---
+++ b1.1/generate-anki-tsv.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Converts .flashcards files to .anki.tsv files.
+#
+# Format of .flashcards:
+# 2 lines per card, the first line is the front, the second is the back.
+#
+# Format of .anki.tsv:
+# front and back sides are separated by tab
+#
+# For each card in .flashcards, this tool generates
+# front-back and back-front entries in .anki.tsv
+# As you add new entries at the end of a .flashcards file,
+# the new entries generated in .anki.tsv will be appended,
+# preserving the previously generated prefix.
+# This is in the hope of better user experience with reimporting in Anki.
+#
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+fatal() {
+    echo >&2 "fatal: $*"
+    exit 1
+}
+
+info() {
+    echo "* $*"
+}
+
+convert_flashcards_to_anki_tsv() {
+    local flashcards_file
+    flashcards_file=$1
+
+    local front back
+
+    while read front; do
+        read back || fatal 'could not read back side of card'
+        printf "%s\t%s\n" "$front" "$back"
+        printf "%s\t%s\n" "$back" "$front"
+    done < <(sed -e 's/[ .]*$//' "$flashcards_file")
+}
+
+for f in *.flashcards; do
+    info "generating for $f ..."
+    convert_flashcards_to_anki_tsv "$f" > "$f.anki.tsv"
+done
---
+++ b1.1/unpaste.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# != 2 ]; then
+    echo "usage: $0 FILE1 FILE2"
+    exit 1
+fi
+
+paste "$1" "$2" | tr '\t' '\n' | sed -e 's/[ .]*$//'
